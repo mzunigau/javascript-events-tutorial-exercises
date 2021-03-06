2b7f2facce06007496bb3665764b007c4baea88e
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-06 00:57:52 +0000

    ejercicio 4

--- exercises/01-alert-on-click/index.js
+++ exercises/01-alert-on-click/index.js
@@ -1,3 +1,7 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
+
+document.getElementById("button2").addEventListener("click", function() {
+	alert("Your second function!");
+});
--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,4 @@
 //Declare your function here
+document.getElementById("hello").addEventListener("click", () => {
+	console.log("Hello World");
+});
